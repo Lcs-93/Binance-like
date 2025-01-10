b877cf2442c0af025412ae2ad17fa2f7d1cf3c4b
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2025-01-10 15:28:35 +0100

    feat: link to / w/ logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,15 @@
 import { RiHome5Line, RiExchangeDollarLine, RiStockLine, RiWallet3Line } from 'react-icons/ri'
 import SidebarLink from './SidebarLink'
 import Trinance from '../../assets/Trinance.png'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <aside className="w-64 h-screen bg-background border-r border-gray">
-      <div className="h-20 flex items-center justify-start gap-2 p-4">
+      <Link to="/" className="h-20 flex items-center justify-start gap-2 p-4">
         <img src={Trinance} alt="" className='w-8 h-6' />
-      <h1 className="text-xl font-bold text-primary">TRINANCE</h1>
-      </div>
+        <h1 className="text-xl font-bold text-primary">TRINANCE</h1>
+      </Link>
 
       <nav className="space-y-2 fixed top-20 w-56 m-4">
         <SidebarLink
